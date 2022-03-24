d3a70f4b02e6920788404bc06f14ba1c63a8f7f7
equationl <equationl@qq.com>
2022-03-24 17:00:42 +0800

    修改：高级页迁移至compose，完成UI迁移

--- app/src/main/java/com/equationl/motortest/compose/ui/AdvancedScreen.kt
+++ app/src/main/java/com/equationl/motortest/compose/ui/AdvancedScreen.kt
@@ -1,5 +1,9 @@
 package com.equationl.motortest.compose.ui
 
+import android.util.Log
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -11,13 +15,18 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.equationl.motortest.R
 import com.equationl.motortest.compose.theme.DarkColors
 import com.equationl.motortest.compose.theme.LightColors
+
+var usingCustomPredefined by mutableStateOf(false)  // TODO 读取初始值
+var usingHighAccuracy by mutableStateOf(false)      // TODO 读取初始值
 
 @Composable
 fun AdvancedScreen(isDarkTheme: Boolean = isSystemInDarkTheme()) {
@@ -45,33 +54,19 @@ fun AdvancedScreen(isDarkTheme: Boolean = isSystemInDarkTheme()) {
 private fun Content() {
     //TODO 主页内容
     Column(modifier = Modifier.fillMaxSize()) {
-        ContentSystemPredefined(false)  // TODO 更改状态
-
+        ContentSystemPredefined()
+        ContentAppPredefined()
+        ContentFreeTest()
+        ContentDiy()
     }
 
 }
 
 @Composable
-private fun ContentSystemPredefined(isCustom: Boolean = false) {
+fun ContentSystemPredefined() {
     Column {
         Text(stringResource(R.string.advanced_text_predefined), modifier = Modifier.padding(start = 8.dp))
-        if (isCustom) {
-            Row {
-                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 8.dp)) {
-                    Text(stringResource(R.string.advanced_text_click))
-                }
-                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 8.dp)) {
-                    Text(stringResource(R.string.advanced_text_doubleClick))
-                }
-                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 8.dp)) {
-                    Text(stringResource(R.string.advanced_text_heavyClick))
-                }
-                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 8.dp)) {
-                    Text(stringResource(R.string.advanced_text_tick))
-                }
-            }
-        }
-        else {
+        if (usingCustomPredefined) {
             var text by remember { mutableStateOf("") }
             Row(
                 horizontalArrangement = Arrangement.Center,
@@ -97,6 +92,273 @@ private fun ContentSystemPredefined(isCustom: Boolean = false) {
                     /*TODO*/
                 }, modifier = Modifier.padding(start = 8.dp)) {
                     Text(stringResource(R.string.advanced_btn_system_customize_start))
+                }
+            }
+        }
+        else {
+            Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
+                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 8.dp)) {
+                    Text(stringResource(R.string.advanced_text_click))
+                }
+                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 8.dp)) {
+                    Text(stringResource(R.string.advanced_text_doubleClick))
+                }
+                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 8.dp)) {
+                    Text(stringResource(R.string.advanced_text_heavyClick))
+                }
+                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 8.dp)) {
+                    Text(stringResource(R.string.advanced_text_tick))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ContentAppPredefined() {
+    Column(modifier = Modifier.padding(start = 8.dp, top = 16.dp)) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(stringResource(R.string.advanced_text_app_predefined))
+            AppPreDropMenu()
+        }
+        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center, modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp)) {
+            Button(onClick = { /*TODO*/ }) {
+                Text(stringResource(R.string.advanced_btn_pre_start))
+            }
+            OutlinedButton(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 8.dp)) {
+                Text(stringResource(R.string.advanced_btn_pre_stop))
+            }
+        }
+    }
+}
+
+@Composable
+fun ContentFreeTest() {
+    val amplitudeStopPoints = arrayOf(0, 23, 46, 69, 92, 115, 139, 162, 185, 208, 231, 225)
+    var amplitudeIsOnStop by remember { mutableStateOf(false) }
+    val amplitudeTextScale: Float by animateFloatAsState(
+        targetValue = if (amplitudeIsOnStop) 1f else 0.8f,
+        animationSpec = spring()
+    )
+
+    Column(modifier = Modifier.padding(start = 8.dp, top = 16.dp)) {
+        Text(stringResource(R.string.advanced_text_free_test))
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Text(stringResource(R.string.advanced_text_amplitud))
+        }
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
+            var sliderPosition by remember { mutableStateOf(if (usingHighAccuracy) 10f else 100f) }
+            Slider(
+                value = sliderPosition,
+                onValueChange = {
+                    sliderPosition = it
+                    if (!usingHighAccuracy && sliderPosition.toInt() in amplitudeStopPoints) {
+                        //TODO 拖动滑块， 振动
+                        amplitudeIsOnStop = true
+                    }
+                    else {
+                        amplitudeIsOnStop = false
+                    }
+                },
+                valueRange = if (usingHighAccuracy) 0f..25f else 0f..255f,
+                steps = if (usingHighAccuracy) 0 else 10,
+                modifier = Modifier.weight(9f)
+            )
+
+            Text(sliderPosition.toInt().toString(), modifier = Modifier
+                .weight(1f)
+                .scale(amplitudeTextScale))
+        }
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Text(stringResource(R.string.advanced_text_rate))
+        }
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
+            var sliderPosition by remember { mutableStateOf(if (usingHighAccuracy) 5f else 50f) }
+            Slider(
+                value = sliderPosition,
+                onValueChange = {
+                    sliderPosition = it
+                    if (!usingHighAccuracy && sliderPosition.toInt() in amplitudeStopPoints) {
+                        //TODO 拖动滑块， 振动
+                    }
+                },
+                valueRange = if (usingHighAccuracy) 0f..10f else 0f..100f,
+                steps = if (usingHighAccuracy) 0 else 10,
+                modifier = Modifier.weight(9f)
+            )
+        }
+        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center, modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp)) {
+            Button(onClick = { /*TODO*/ }) {
+                Text(stringResource(R.string.advanced_btn_free_start))
+            }
+            OutlinedButton(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 8.dp)) {
+                Text(stringResource(R.string.advanced_btn_free_stop))
+            }
+        }
+
+    }
+}
+
+@Composable
+fun ContentDiy() {
+    // TODO diy
+
+    Column(modifier = Modifier.padding(start = 8.dp, top = 16.dp)) {
+        Text(stringResource(R.string.advanced_text_diy))
+        var timingsText by remember { mutableStateOf("") }
+        var amplitudesText by remember { mutableStateOf("") }
+        var repeatText by remember { mutableStateOf("") }
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                value = timingsText,
+                onValueChange = { timingsText = it },
+                label = { Text(stringResource(R.string.advanced_edit_diy_timings)) },
+                trailingIcon = {
+                    if (timingsText.isNotEmpty()) {
+                        IconButton(onClick = { timingsText = "" }) {
+                            Icon(painter = painterResource(android.R.drawable.ic_menu_close_clear_cancel),
+                                contentDescription = stringResource(R.string.advanced_edit_systemCustomize_clr_description)
+                            )
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 8.dp)
+            )
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                value = amplitudesText,
+                onValueChange = { amplitudesText = it },
+                label = { Text(stringResource(R.string.advanced_edit_diy_amplitudes)) },
+                trailingIcon = {
+                    if (amplitudesText.isNotEmpty()) {
+                        IconButton(onClick = { timingsText = "" }) {
+                            Icon(painter = painterResource(android.R.drawable.ic_menu_close_clear_cancel),
+                                contentDescription = stringResource(R.string.advanced_edit_systemCustomize_clr_description)
+                            )
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 8.dp)
+            )
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                value = repeatText,
+                onValueChange = { repeatText = it },
+                label = { Text(stringResource(R.string.advanced_edit_diy_repeat)) },
+                trailingIcon = {
+                    if (repeatText.isNotEmpty()) {
+                        IconButton(onClick = { repeatText = "" }) {
+                            Icon(painter = painterResource(android.R.drawable.ic_menu_close_clear_cancel),
+                                contentDescription = stringResource(R.string.advanced_edit_systemCustomize_clr_description)
+                            )
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 8.dp)
+            )
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        ) {
+            Button(onClick = {
+                Log.i("test", "ContentDiy: 点击按钮")
+            }) {
+                Text(
+                    stringResource(R.string.advanced_btn_diy_start),
+                    modifier = Modifier.clickable {
+                        Log.i("test", "ContentDiy: 点击文字")
+                    }
+                )
+                Icon(
+                    painter = painterResource(R.drawable.ic_btn_menu_down),
+                    contentDescription = "",
+                    modifier = Modifier.padding(start = 8.dp).clickable {
+                        Log.i("test", "ContentDiy: 点击图标")
+                    }
+                )
+            }
+
+            OutlinedButton(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 8.dp)) {
+                Text(stringResource(R.string.advanced_btn_diy_stop))
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun AppPreDropMenu() {
+    val options = stringArrayResource(R.array.advanced_spinner_pre)
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOptionText by remember { mutableStateOf(options[0]) }
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = !expanded
+        }
+    ) {
+        OutlinedTextField( // TODO 更改样式
+            readOnly = true,
+            value = selectedOptionText,
+            onValueChange = { selectedOptionText = it },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded
+                )
+            },
+            colors = ExposedDropdownMenuDefaults.textFieldColors()
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {
+                expanded = false
+            }
+        ) {
+            options.forEachIndexed { index, selectionOption ->
+                DropdownMenuItem(
+                    onClick = {
+                        selectedOptionText = selectionOption
+                        expanded = false
+                        /*TODO 更改预设模式*/
+                    },
+                    enabled = options.lastIndex != index
+                ) {
+                    Text(text = selectionOption)
                 }
             }
         }
@@ -154,7 +416,12 @@ private fun TopBarDropdownMenu() {
         }) {
             TopBarMenuItem(
                 text = stringResource(R.string.advanced_menu_high_accuracy),
-                onCheckItem = {/*TODO 使用高精度*/})
+                onCheckItem =
+                {
+                    // TODO 使用高精度， 保存数值
+                    usingHighAccuracy = it
+                },
+                usingHighAccuracy)
         }
 
         Divider()
@@ -164,7 +431,11 @@ private fun TopBarDropdownMenu() {
         }) {
             TopBarMenuItem(
                 text = stringResource(R.string.advanced_action_run_background),
-                onCheckItem = {/*TODO 后台振动*/})
+                onCheckItem = {
+                    // TODO 后台振动， 保存数值
+                },
+            false
+            )  // TODO 读取数值
         }
 
         Divider()
@@ -174,14 +445,18 @@ private fun TopBarDropdownMenu() {
         }) {
             TopBarMenuItem(
                 text = stringResource(R.string.advanced_action_customize_system_default),
-                onCheckItem = {/*TODO 自定义系统预设*/})
+                onCheckItem = {
+                    // TODO 自定义系统预设， 保存数值
+                    usingCustomPredefined = it
+                },
+                usingCustomPredefined)
         }
     }
 }
 
 @Composable
-fun TopBarMenuItem(text: String, onCheckItem: (Boolean) -> Unit) {
-    var checkedState by remember { mutableStateOf(false) }
+fun TopBarMenuItem(text: String, onCheckItem: (Boolean) -> Unit, defaultCheckState: Boolean) {
+    var checkedState by remember { mutableStateOf(defaultCheckState) }
     Row(
         verticalAlignment = Alignment.CenterVertically
     ) {
